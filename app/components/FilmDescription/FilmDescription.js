28bda6e3e6ca43c9c61ac8a8c5d375ec849491dd
HolaShola <xxjxxx@mail.ru>
2017-09-18 23:14:27 +0300

    FilmDescription refactoring

--- app/components/FilmDescription/FilmDescription.js
+++ app/components/FilmDescription/FilmDescription.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './FilmDescription.css';
+import AppTitle from '../AppTitle';
+import Button from '../Button';
 
 class FilmDescription extends Component {
     render(props) {
@@ -7,34 +9,30 @@ class FilmDescription extends Component {
             <div className="header">
                 <div className="header_label">
                     <div className="header_label_up">
-                        <p>nextflixroulette</p>
-                        <button>search</button>	
+                        <AppTitle />
+                        <Button type="submit" text="search" />
                     </div>	
                     <div className="header_down">
                         <div className="header_img">
-                            <img src="https://netflixroulette.net/api/posters/880640.jpg" />
+                            <img src={this.props.posterUrl} />
                         </div>
                         <div className="header_img_description">
                             <div className="show_title">
-                                <p>Pulp Fiction</p>
+                                <p>{this.props.show_title}</p>
                             </div>
-                            <div className="film_rating">4.1</div>
+                            <div className="film_rating">{this.props.rating}</div>
                             <div>
-                                <p className="category">Oscar-winning</p>
+                                <p className="category">{this.props.category}</p>
                             </div>
                             <div>
-                                <p className="release_year film_duration">1994 154 min</p>
+                                <p className="release_year film_runtime">{this.props.release_year}       {this.props.runtime}</p>
                             </div>
-                            <div className="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                            cillum dolore eu fugiat nulla pariatur. Exc.</div>
+                            <div className="description">
+                                {this.props.summary}
+                            </div>
                             <div className="director_cast_list">
-                                <p>Director: Quentin Tarantino</p>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aliquid
-                                    consectetur cum dolore eligendi illo nisi perspiciatis quibusdam
-                                </p>
+                                <p>{this.props.director}</p>
+                                <p>{this.props.show_cast}</p>
                             </div>
                         </div>
                     </div>
