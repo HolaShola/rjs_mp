dcc5669ac57df04d133871269fd8bc54d777137b
HolaShola <xxjxxx@mail.ru>
2018-02-28 09:39:07 +0300

    reducers deleted

--- app/reducers/CurrentFilmReducer.js
+++
@@ -1,7 +0,0 @@
-export default function CurrentFilmReducer(state = 0, action) {
-  switch (action.type) {
-    case 'FETCH_CURRENT_MOVIE_RECEIVE':
-      return action.payload;
-    default: return state;
-  }
-}
--- app/reducers/isFetchingReducer.js
+++
@@ -1,9 +0,0 @@
-const initialState = false;
-
-export default function SearchReducer(state = initialState, action) {
-  switch (action.type) {
-    case 'FETCH_MOVIES_REQUEST':
-      return action.payload;
-    default: return state;
-  }
-}
